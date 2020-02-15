1c560ef3c09aced111377f30b3c15395b4475e86
Vlad V <summervibeswith9pm@gmail.com>
2020-02-15 17:04:23 +0200

    Fix typo

--- pages/index.js
+++ pages/index.js
@@ -391,33 +391,33 @@ const Index = () => (
         </p>
       </div>
     </section>
-    <section className="corousel">
-      <button type="button" className="corousel__control">
+    <section className="carousel">
+      <button type="button" className="carousel__control">
         <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" clipRule="evenodd" d="M34.8297 18.6013H5.12463L19.3955 32.942L17.7539 34.5917L0.680359 17.4347L17.7562 0.27533L19.3979 1.925L5.12463 16.268H34.8297V18.6013Z" fill="#333333" />
         </svg>
       </button>
-      <div className="corousel__slide">
-        <div className="corousel__slide-content">
-          <h3 className="corousel__slide-content-header">
+      <div className="carousel__slide">
+        <div className="carousel__slide-content">
+          <h3 className="carousel__slide-content-header">
             Then they show that show to the people who make shows
           </h3>
-          <p className="corousel__slide-content-description">
+          <p className="carousel__slide-content-description">
             The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men.
             Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he
             is truly his brother’s.
           </p>
         </div>
-        <div className="corousel__pagination">
-          <button type="button" className="corousel__pagination-control" />
-          <button type="button" className="corousel__pagination-control" />
-          <button type="button" className="corousel__pagination-control corousel__pagination-control--active" />
-          <button type="button" className="corousel__pagination-control" />
-          <button type="button" className="corousel__pagination-control" />
-          <button type="button" className="corousel__pagination-control" />
+        <div className="carousel__pagination">
+          <button type="button" className="carousel__pagination-control" />
+          <button type="button" className="carousel__pagination-control" />
+          <button type="button" className="carousel__pagination-control carousel__pagination-control--active" />
+          <button type="button" className="carousel__pagination-control" />
+          <button type="button" className="carousel__pagination-control" />
+          <button type="button" className="carousel__pagination-control" />
         </div>
       </div>
-      <button type="button" className="corousel__control">
+      <button type="button" className="carousel__control">
         <svg width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" clipRule="evenodd" d="M34.1494 17.4347L17.0735 34.594L15.4319 32.9443L29.7028 18.6037H0V16.2703H29.7051L15.4319 1.925L17.0735 0.27533L34.1494 17.4347Z" fill="#333333" />
         </svg>
