6d145054de92d91cd8672e119cc816c9451bda0f
Vlad V <summervibeswith9pm@gmail.com>
2020-06-13 15:10:31 +0300

    Added mobile view for stats and portfolio

--- pages/index.js
+++ pages/index.js
@@ -420,7 +420,7 @@ const Index = () => (
       </div>
     </section>
     <section className="carousel">
-      <button type="button" aria-label="Go to previous image" className="flat-button">
+      <button type="button" aria-label="Go to previous image" className="flat-button d-none d-block-lg">
         <svg focusable="false" width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" clipRule="evenodd" d="M34.8297 18.6013H5.12463L19.3955 32.942L17.7539 34.5917L0.680359 17.4347L17.7562 0.27533L19.3979 1.925L5.12463 16.268H34.8297V18.6013Z" fill="#333333" />
         </svg>
@@ -448,7 +448,7 @@ const Index = () => (
           <button type="button" aria-label="Go to 8 image" className="carousel__pagination-control" />
         </div>
       </div>
-      <button type="button" aria-label="Go to next image" className="flat-button">
+      <button type="button" aria-label="Go to next image" className="flat-button d-none d-block-lg">
         <svg focusable="false" width="35" height="35" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fillRule="evenodd" clipRule="evenodd" d="M34.1494 17.4347L17.0735 34.594L15.4319 32.9443L29.7028 18.6037H0V16.2703H29.7051L15.4319 1.925L17.0735 0.27533L34.1494 17.4347Z" fill="#333333" />
         </svg>
