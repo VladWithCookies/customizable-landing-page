2f2aa2bcc2e650fcfc219b4b38f1ea0af321b123
Vlad V <summervibeswith9pm@gmail.com>
2020-06-14 13:43:46 +0300

    Added mobile view for carousel, our team, partners and pricing plans sections

--- pages/index.js
+++ pages/index.js
@@ -611,9 +611,9 @@ const Index = () => (
     </section>
     <section className="partners">
       <img className="partners__logo" src="https://xn--80aed5aobb1a.xn--p1ai/wp-content/uploads/ford-text-logo-black-2048x2048.png" alt="Ford logo" />
-      <img className="partners__logo d-block-sm" src="https://xn--80aed5aobb1a.xn--p1ai/wp-content/uploads/ford-text-logo-black-2048x2048.png" alt="Ford logo" />
-      <img className="partners__logo d-block-md" src="https://xn--80aed5aobb1a.xn--p1ai/wp-content/uploads/ford-text-logo-black-2048x2048.png" alt="Ford logo" />
-      <img className="partners__logo d-block-lg" src="https://xn--80aed5aobb1a.xn--p1ai/wp-content/uploads/ford-text-logo-black-2048x2048.png" alt="Ford logo" />
+      <img className="partners__logo" src="https://xn--80aed5aobb1a.xn--p1ai/wp-content/uploads/ford-text-logo-black-2048x2048.png" alt="Ford logo" />
+      <img className="partners__logo" src="https://xn--80aed5aobb1a.xn--p1ai/wp-content/uploads/ford-text-logo-black-2048x2048.png" alt="Ford logo" />
+      <img className="partners__logo" src="https://xn--80aed5aobb1a.xn--p1ai/wp-content/uploads/ford-text-logo-black-2048x2048.png" alt="Ford logo" />
     </section>
     <section className="contact-us">
       <div className="contact-us__form-container">
