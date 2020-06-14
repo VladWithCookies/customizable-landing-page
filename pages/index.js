c0ebf00796d78a143823ebcec9e0a781d996608b
Vlad V <summervibeswith9pm@gmail.com>
2020-06-14 17:24:56 +0300

    Added mobile view for contact us and footer

--- pages/index.js
+++ pages/index.js
@@ -677,7 +677,7 @@ const Index = () => (
             info@mail.com
           </a>
         </address>
-        <a href="#top" className="flat-button mt-55" aria-label="Back to top">
+        <a href="#top" className="flat-button mt-55 mb-30" aria-label="Back to top">
           <svg focusable="false" width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fillRule="evenodd" clipRule="evenodd" d="M16.354 7.89597L15.647 8.60297L8.5 1.45697L1.354 8.60297L0.647003 7.89597L8.5 0.0429688L16.354 7.89597Z" fill="white" />
           </svg>
